c4dead9521760bcf5715d2dd6bd7dccfe23fb6d0
hyungtack <84324343+Polo-Kim@users.noreply.github.com>
2022-04-10 14:06:40 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/frogkim93/bmb/BowlingManagerBackApplication.java
@@ -0,0 +1,13 @@
+package com.frogkim93.bmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingManagerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingManagerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frogkim93/bmb/BowlingManagerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frogkim93.bmb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingManagerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
